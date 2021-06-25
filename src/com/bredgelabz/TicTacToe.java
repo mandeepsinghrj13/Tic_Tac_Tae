b886f0da2c346e0cf57792e26e48f3f1aef743e5
mandeep <singh.mr.1996@gmail.com>
2021-06-25 09:25:42 +0530

    Added: Uc1_Creating_Tic_Tac_Toe_Board

--- src/com/bredgelabz/TicTacToe.java
+++ src/com/bredgelabz/TicTacToe.java
@@ -1,11 +1,20 @@
 package com.bredgelabz;
-
+import java.util.Iterator;
 public class TicTacToe 
 {
-
 	public static void main(String[] args) 
 	{
 		System.out.println("Tic Tac Toe");
+		board();
 	}
-
+	public static void board() //creating a method
+	{	
+		int i;
+		char[] element;
+		element = new char[10];
+		for(i=1; i<10; i++) 
+		{
+			element[i]='-';
+		}
+	}
 }
