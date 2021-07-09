fe392345e19a2c6852f2479985f530da37bf3081
mandeep <singh.mr.1996@gmail.com>
2021-07-09 15:54:16 +0530

    player choose a letter

--- src/com/bredgelabz/TicTacToe.java
+++ src/com/bredgelabz/TicTacToe.java
@@ -1,20 +1,45 @@
-package com.bredgelabz;
-import java.util.Iterator;
+package com.bredgelabz; //creating package
+import java.util.*;
 public class TicTacToe 
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("Tic Tac Toe");
-		board();
-	}
-	public static void board() //creating a method
-	{	
-		int i;
-		char[] element;
-		element = new char[10];
-		for(i=1; i<10; i++) 
-		{
-			element[i]='-';
+	private static char[] Board() {
+		char[] boardArray = new char[11];
+		for(char c : boardArray) {
+			c = ' ';
 		}
+		return boardArray;
 	}
+	
+private static void ChooseLetter() {
+	
+	char firstCharacterInput = ' ';
+	char computerCharacterInput = ' ';
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter input Mr. player");
+	System.out.println("Enter 'X' or 'O' : ");
+	firstCharacterInput = sc.next().charAt(0);
+	
+	switch (firstCharacterInput) {
+	case 'X': {
+		firstCharacterInput = 'X';
+		computerCharacterInput = 'O';
+		break;
+	}
+	case 'O': {
+		firstCharacterInput = 'O';
+		computerCharacterInput = 'X';
+		break;
+	}
+	default:
+	}
+	
+	System.out.println("User input is : " + firstCharacterInput + " and computer input is : " + computerCharacterInput);
+	sc.close();
+}
+
+public static void main(String[] args) {		
+	char[] boardArray = Board();
+	ChooseLetter();
+}
 }
