71fa338b7ebabe44679cfbcb904d741ae00386b2
mandeep <singh.mr.1996@gmail.com>
2021-07-09 19:30:18 +0530

    check for free space

--- src/com/bredgelabz/TicTacToe.java
+++ src/com/bredgelabz/TicTacToe.java
@@ -3,10 +3,9 @@ package com.bredgelabz; //creating package
 import java.util.*;
 
 public class TicTacToe {
-	@SuppressWarnings("unused")
 	private static char[] boardArray;
 	private static int[] boardArrayIndex = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	
+
 	// Returns the board in the form of a 1D array
 	private static char[] Board() {
 		char[] dummyBoardArray = new char[10];
@@ -15,59 +14,87 @@ public class TicTacToe {
 		}
 		return dummyBoardArray;
 	}
-	
-	// boardArrayIndex array
-		@SuppressWarnings("unused")
-		private static boolean IsEmpty(int index) {
-			if (boardArrayIndex[index] == 0) {
-				return true;
-			} else {
-				return false;
-			}
+
+	private static boolean IsEmpty(int index) {
+		if (boardArrayIndex[index] == 0) {
+			return true;
+		} else {
+			return false;
 		}
-			// Allows the user to input 'X' or 'O'
-		
-		
+	}
+		// Allows the user to 'X' or 'O'
+	
+ 
 private static void ChooseLetter() {
 	
+	@SuppressWarnings("unused")
 	char firstCharacterInput = ' ';
 	char computerCharacterInput = ' ';
-	Scanner sc = new Scanner(System.in);
-	
-	System.out.println("Enter input Mr. player");
-	System.out.println("Enter 'X' or 'O' : ");
-	firstCharacterInput = sc.next().charAt(0);
-	
-	switch (firstCharacterInput) {
-	case 'X': {
-		firstCharacterInput = 'X';
-		computerCharacterInput = 'O';
-		break;
+	try (Scanner sc = new Scanner(System.in)) {
+		int positionOfMove;
+		System.out.println("Time to give input Mr. Player : ");
+		while (true) {
+			System.out.println("Enter the position you want to give input in (from 1-9) : ");
+			positionOfMove = sc.nextInt();
+			if (positionOfMove >= 1 && positionOfMove <= 9) {
+				if (IsEmpty(positionOfMove) == false) {
+					System.out.println("The position is not empty. Please give other position input.");
+					continue;
+				}
+				break;
+			} else {
+				System.out.println("The position is not empty. Please give other position input.");
+				continue;
+			}
+		}
+		
+		
+		boardArrayIndex[positionOfMove] = 1;
+		while (true) {
+			System.out.println("Enter 'X' or 'O' down below : ");
+			char playerCharacterInput = sc.next().charAt(0);
+			playerCharacterInput = sc.next().charAt(0);
+			playerCharacterInput = Character.toUpperCase(playerCharacterInput);
+			boardArray[positionOfMove] = playerCharacterInput;
+			switch (playerCharacterInput) {
+			case 'X': {
+				playerCharacterInput = 'X';
+				computerCharacterInput = 'O';
+				break;
+			}
+			case 'O': {
+				playerCharacterInput = 'O';
+				computerCharacterInput = 'X';
+				break;
+			}
+			default: {
+				System.out.println("Please enter a valid input");
+				
+				
+				continue;
+			}
+			}
+
+			System.out.println("User input is : " + playerCharacterInput + " so the computer input will be : "
+					+ computerCharacterInput);
+			break;
+		}
 	}
-	case 'O': {
-		firstCharacterInput = 'O';
-		computerCharacterInput = 'X';
-		break;
-	}
-	default:
-	}
-	
-	System.out.println("User input is : " + firstCharacterInput + " and computer input is : " + computerCharacterInput);
-	sc.close();
 }
 
 
+
 private static void ShowBoard(char[] boardArray) {
-	System.out.println("-----THE BOARD-----");
-	System.out.println("___________________");
+	System.out.println("THE BOARD");
+	System.out.println(" ");
 	System.out.print("|  ");
 	for (char i = 1; i < 3; i++) {
 		System.out.print(boardArray[i] + "  |  ");
 	}
-	System.out.print(boardArray[3] + "  ");
+	System.out.print( boardArray[3] + "  ");
 	System.out.print("|");
 	System.out.println();
-	System.out.println("___________________");
+	System.out.println(" ");
 	System.out.print("|  ");
 	for (char i = 4; i < 6; i++) {
 		System.out.print(boardArray[i] + "  |  ");
@@ -75,14 +102,14 @@ private static void ShowBoard(char[] boardArray) {
 	System.out.print(boardArray[6] + "  ");
 	System.out.print("|");
 	System.out.println();
-	System.out.println("___________________");
+	System.out.println(" ");
 	System.out.print("|  ");
 	for (char i = 7; i < 9; i++) {
 		System.out.print(boardArray[i] + "  |  ");
 	}
 	System.out.print(boardArray[9] + "  ");
 	System.out.println("|");
-	System.out.print("___________________");
+	System.out.print(" "); 
 }
 public static void main(String[] args) {		
 	char[] boardArray = Board();
